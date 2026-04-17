36d71fe54536fd0195c0c2892aad07e5573ca4a3
Melvin <melvin@lluvialive.com>
2026-04-17 08:38:12 +0000

    auto-commit for 9a783354-cb37-49dd-bb5d-5da68d3e7548

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import "@/App.css";
+import "./App.css";
 import { UserProvider, useUser } from './contexts/UserContext';
 import LoginPage from './pages/LoginPage';
 import Dashboard from './pages/Dashboard';
